8cab27bf0c4423f31b4d4046a0c73487f72921b9
IvanNugiban <aa1245541@gmail.com>
2022-07-19 10:30:52 +0300

    Added ot found route

--- src/routes/WeatherRoutes.tsx
+++ src/routes/WeatherRoutes.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route, Routes} from "react-router-dom";
+import {Navigate, Route, Routes} from "react-router-dom";
 import WeatherNow from "../components/Main/WeatherMenu/WeatherNow/WeatherNow";
 import FutureWeather from "../components/Main/WeatherMenu/FutureWeather/FutureWeather";
 import {ICities} from "../types/ICity";
@@ -17,6 +17,7 @@ const WeatherRoutes = ({city, weather, weatherType} : IProps) => {
            <Route path='/now' element={<WeatherNow city={city} weather={weather}/> }/>
            <Route path='/hourly' element={<FutureWeather city={city} weather={weather} weatherType={weatherType}/> }/>
            <Route path='/daily' element={<FutureWeather  city={city} weather={weather} weatherType={weatherType}/> }/>
+           <Route path='*' element={<Navigate to='/now' replace/> }/>
        </Routes>
     );
 };
